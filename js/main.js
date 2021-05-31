5028d1de6285861d2929617bebfef94dd6d47be5
Starfisher <supremeeworker.com>
2021-05-31 17:40:43 +0300

    Правки на линтер

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 function getRandomNumber(min, max) {
   if ((min < 0) || (max < 0)) {
-    console.log('Введите значения больше или равное 0');
-    return;
+    return 'Введите значения больше или равное 0';
   }
 
-  let value = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(value);
-  return value;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function checkMaxStringLength(string, maxLength) {
   if (string.length > maxLength) {
-    console.log('Допустимое количество символов - ' + maxLength);
     return false;
   }
 
   return true;
 }
+
+getRandomNumber(1, 5);
+checkMaxStringLength('12345', 5);

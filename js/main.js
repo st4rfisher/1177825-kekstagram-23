785d6e07970355ab922eb4b9d56245b109c49242
Starfisher <supremeeworker@gmail.com>
2021-06-23 14:51:00 +0300

    Реализован модуль рендера миниатюр

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-import {generateCommentAttributes} from './generate-comment-attributes.js';
-import {generatePhotoAttributes} from './generate-photo-attributes.js';
-
-generateCommentAttributes();
-generatePhotoAttributes();
+import './render-thumbnails.js';

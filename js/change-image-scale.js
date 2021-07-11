70589f6a23510540c67c2066efe560d6b95b7997
Starfisher <vladokforever@yandex.ru>
2021-07-11 22:05:33 +0300

    Реализация масштабирования и начало реализации фильтров

---
+++ js/change-image-scale.js
@@ -0,0 +1,30 @@
+import {uploadFileOverlay} from './form.js';
+
+const scaleControlSmaller = uploadFileOverlay.querySelector('.scale__control--smaller');
+const scaleControlBigger = uploadFileOverlay.querySelector('.scale__control--bigger');
+const scaleControlValue = uploadFileOverlay.querySelector('.scale__control--value');
+const previewImage = uploadFileOverlay.querySelector('.img-upload__preview img');
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+const STEP = 25;
+
+scaleControlValue.setAttribute('value', `${MAX_SCALE}%`);
+previewImage.style.transform = `scale(${MAX_SCALE}%, ${MAX_SCALE}%)`;
+
+scaleControlBigger.addEventListener('click', () => {
+  const value = Number(scaleControlValue.value.replace(/[^-0-9-.]/, ''));
+  if(value < MAX_SCALE) {
+    scaleControlValue.setAttribute('value', (`${value + STEP}%`));
+    previewImage.style.transform = `scale(${value + STEP}%)`;
+  }
+});
+
+scaleControlSmaller.addEventListener('click', () => {
+  const value = Number(scaleControlValue.value.replace(/[^-0-9-.]/, ''));
+  if(value > MIN_SCALE) {
+    scaleControlValue.setAttribute('value', (`${value - STEP}%`));
+    previewImage.style.transform = `scale(${value - STEP}%)`;
+  }
+});
+
+
--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ import './render-thumbnails.js';
 import './render-full-image.js';
 import './form.js';
 import './validate-form.js';
+import './change-image-scale.js';
+import './applying-effects.js';
